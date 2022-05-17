ac4845df762bc0355ca13b99284a56a37a188e0e
lucagad <90861515+lucagad@users.noreply.github.com>
2022-05-17 16:13:44 +0200

    {Vue} Added: AlbumComponent with Dynamics data

--- src/components/SecondaryComponent/AlbumComponent.vue
+++ src/components/SecondaryComponent/AlbumComponent.vue
@@ -1,13 +1,52 @@
 <template>
-  <div>ALBUM CARD</div>
+  <div class="card_container">
+    <img :src="Album.poster" :alt="Album.title">
+    <h5>{{Album.title}}</h5>
+    <span>{{Album.author}}</span>
+    <span>{{Album.year}}</span>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name:'AlbumComponent'
+  name:'AlbumComponent',
+
+  props: {
+    Album : Object
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+
+@import '../../assets/style/general';
+@import '../../assets/style/mixins';
+@import '../../assets/style/utils';
+@import '../../assets/style/vars';
+
+.card_container{
+  @include flex-column-cnt();
+  background-color: $secondary-color;
+  width: 100%;
+  height: calc(100% - 30px);
+  
+  padding-top: 10px;
+  margin: 15px 0;
+  padding-bottom: 50px;
+
+  img{
+    width: 100%;
+    padding: 10px;
+  }
+  h5{
+    padding: 20px 0;
+    text-transform: uppercase;
+  }
+
+  span{
+    color: grey;
+  }
+}
 
 </style>
