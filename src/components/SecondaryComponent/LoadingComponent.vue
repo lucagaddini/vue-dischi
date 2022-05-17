e6404cf4142d74ba61ab8e5751653c1770c4e805
lucagad <90861515+lucagad@users.noreply.github.com>
2022-05-17 16:12:01 +0200

    {Vue} Added: LoadingComponent

--- src/components/SecondaryComponent/LoadingComponent.vue
+++ src/components/SecondaryComponent/LoadingComponent.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>LOADER</div>
+  <div class="loader_container">
+    <div class="loader">
+      <img src="../../assets/img/logo.png" alt="Logo" />
+    </div>
+    <div class="title">{{loaderName}}</div> 
+  </div>
 </template>
 
 <script>
 export default {
-  name:'LoadingComponent'
+  name:'LoadingComponent',
+
+  props: {
+
+    loaderName: String,
+
+  }
 }
 </script>
 
@@ -14,5 +25,40 @@ export default {
 @import '../../assets/style/mixins';
 @import '../../assets/style/utils';
 @import '../../assets/style/vars';
+
+.loader_container{
+  width: 100%;
+  height: 100vh;
+  @include flex-column-cnt();
+}
+
+.title{
+  padding: 25px;
+  font-size: 50px;
+}
+
+.loader {
+    border: 16px solid lighten($primary-color, 20%); /* Light grey */
+    border-top: 16px solid $logo-primary-color; /* Dark Green */
+    border-radius: 50%;
+    width: 137px;
+    height: 137px;
+    animation: spinloader 2s linear infinite;
+}
+.loader img{
+    height : 105px;
+    width : 105px;
+    animation: spinlogo 2s linear infinite;
+}
+@keyframes spinloader {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+
+@keyframes spinlogo {
+    0% { transform: rotate(360deg); }
+    100% { transform: rotate(0deg); }
+}
+
 
 </style>
