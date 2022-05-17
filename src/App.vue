ee4e2f3faca9d3b072614b1d406075c197da2d6f
lucagad <90861515+lucagad@users.noreply.github.com>
2022-05-17 16:13:08 +0200

    {Vue} Fixed: API Call

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <HeaderComponent />
-    <MainComponent />
+    <MainComponent/>
   </div>
 </template>
 
@@ -9,32 +9,11 @@
 import HeaderComponent from "./components/HeaderComponent.vue";
 import MainComponent from "./components/MainComponent.vue";
 
-import axios from 'axios';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 
 
 export default {
-    components: { HeaderComponent, MainComponent },
-    data(){
-      return{
-        baseURL:'https://flynn.boolean.careers/exercises/api/array/music',
-        albumArray:[],
-        isLoaded : false
-      }
-    },
-    mounted(){
-      this.getAPI()
-    },
-    methods:{
-      getAPI(){
-        axios.get(this.baseURL)
-        .then(r => {
-          this.albumArray = r.data.response;
-          console.log(r.data);
-          this.isLoaded = true
-        })
-      }
-    }
+  components: { HeaderComponent, MainComponent },
 }
 </script>
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,55 @@
 <template>
-  <div>MAIN</div>
+  <div class="main_wrapper">
+
+    <div v-if="isLoaded" class="album_container container">
+
+      <div class="row">
+        <div 
+        v-for=" (album,index) in albumArray" 
+        :key="`album-${index}`" 
+        class="col-6 col-md-4 col-lg-2"> <AlbumComponent :album="album" /> </div>
+      </div>
+
+    </div>
+
+    <div class="container" v-else>
+      <LoadingComponent loaderName="LOADING MUSIC"/>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import AlbumComponent from "./SecondaryComponent/AlbumComponent.vue";
+import LoadingComponent from "./SecondaryComponent/LoadingComponent.vue";
+
+import axios from 'axios';
+
 export default {
-  name:'MainComponent'
+    name: "MainComponent",
+
+    components: { AlbumComponent, LoadingComponent },
+
+    data(){
+      return{
+        baseURL:'https://flynn.boolean.careers/exercises/api/array/music',
+        albumArray:[],
+        isLoaded : false
+      }
+    },
+    mounted(){
+      this.getAPI()
+    },
+    methods:{
+      getAPI(){
+        axios.get(this.baseURL)
+        .then(r => {
+          this.albumArray = r.data.response;
+          console.log(r.data);
+          this.isLoaded = true
+        })
+      }
+    }
 }
 </script>
 
@@ -14,5 +59,19 @@ export default {
 @import '../assets/style/mixins';
 @import '../assets/style/utils';
 @import '../assets/style/vars';
+
+.main_wrapper{
+  background-color: $primary-color;
+  min-height: 100vh;
+  height: 100%;
+  color: white;
+
+  .album_container{
+    @include flex-cnt();
+    padding-top: 70px;
+    height: 100%;
+    width: 100%;
+  }
+}
 
 </style>
